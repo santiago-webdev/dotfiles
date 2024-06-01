0cc39a5b9629cfe14309d8d3a72419bb76c55df6
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2024-06-01 18:27:12 -0300

    feat: no need to force openwebui startup

--- scripts/.local/bin/warm-up-containers.sh
+++ scripts/.local/bin/warm-up-containers.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-systemctl --user start open-webui-quadlet
-
 # Running the distrobox command in the background and disconnecting it from the script/terminal
 nohup distrobox enter general > /dev/null 2>&1 &
 disown
